03dacf8cb318944659e20ec1bb91573116d03465
Elf Sundae <elf.sundae@gmail.com>
2022-06-14 11:07:15 +0800

    existingClasses

--- examples/demo.php
+++ examples/demo.php
@@ -4,6 +4,11 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Elfsundae\Laravel\FacadePhpdocGenerator;
 
+function existingClasses($classes)
+{
+    return array_filter($classes, 'class_exists');
+}
+
 if (class_exists(\Illuminate\Log\LogManager::class)) {
     echo FacadePhpdocGenerator::make(\Illuminate\Log\LogManager::class);
 
@@ -15,9 +20,9 @@ if (class_exists(\Illuminate\Log\LogManager::class)) {
 
 echo FacadePhpdocGenerator::make(\Illuminate\Translation\Translator::class);
 
-echo FacadePhpdocGenerator::make([
+echo FacadePhpdocGenerator::make(existingClasses([
     \Illuminate\Auth\AuthManager::class,
     \Illuminate\Contracts\Auth\Factory::class,
     \Illuminate\Contracts\Auth\Guard::class,
     \Illuminate\Contracts\Auth\StatefulGuard::class,
-]);
+]));
